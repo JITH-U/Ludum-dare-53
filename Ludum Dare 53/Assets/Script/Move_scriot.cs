733be946b47cd6a7a342a5b06b8ac9158b66e767
EswaranGame <propraveen117@gmail.com>
2023-04-29 14:05:26 +0530

    qwe

--- Ludum Dare 53/Assets/Script/Move_scriot.cs
+++ Ludum Dare 53/Assets/Script/Move_scriot.cs
@@ -6,11 +6,17 @@ public class Move_scriot : MonoBehaviour
 {
 
     [SerializeField] float speed;
-    private float distance;
+    private float movement;
    
     void Update()
     {
-        distance = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * speed * distance * Time.deltaTime);
+        movement = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speed * movement * Time.deltaTime);
+
+        float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime; 
+        transform.Translate(0, 0, translation); 
+
+
+
     }
 }
